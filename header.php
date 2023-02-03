772205abe4aa14474ffd1c739e96f9ceefd10cdc
RoshniKR <krroshni02@gmail.com>
2023-02-03 16:19:36 +0530

    initial commit

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Medical Management System</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<style>
+		.error {color: #FF0000;}
+	</style>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="header_top"  >
+			<!-- <a href="index.php"><img src="img/dash2.jpg" alt="logo.png img"></a> -->
+			<!--<img src="img/newimage1.jpg">-->
+			<span style="font-size:55px;font-family:Lucida Sans;color:black;font-weight:45px;margin-left:533px;position:absolute;top:125px;visibility:inherit;">Online</span>
+		</div>
+
+	<!-- 	this is for menu -->
+	<div class="navbar navbar-default nav">
+		<nav class="menu">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="medical_college.php">Medical College</a></li>
+				<li><a href="doctorinfo.php">Medical Doctors</a></li>
+				<li><a href="about.php">About Us</a></li>
+				<li><a href="contactus.php">Contact Us</a></li>
+				<li><a href="signin.php">User Login</a></li>
+				
+			</ul>
+		</nav>
+	</div>
+	
